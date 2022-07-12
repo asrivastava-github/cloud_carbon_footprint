e75a7a144778c5167f0393f50f50a149cb6a1c25
Avinash Chandra Srivastava <avinashchandra.srivastava@edfenergy.com>
2022-07-12 09:41:26 +0100

    App up

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,9 @@
 variable "environment" {}
 variable "prefix" {}
-variable "ami_id" {}
 variable "instance_type" {}
 variable "key_name" {}
 variable "application" {}
 variable "allowed_cidr_blocks" {}
 variable "bucket_functions" {}
+variable "app_ami_id" {}
+variable "web_ami_id" {}
